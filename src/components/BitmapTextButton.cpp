09530c37376b84e38889f293773708a974e08d7e
Gal Sasson <sasgal@gmail.com>
2015-01-20 10:21:56 -0500

    add BitmapTextButton

---
+++ src/components/BitmapTextButton.cpp
@@ -0,0 +1,38 @@
+//
+//  BitmapTextButton.cpp
+//  AnimationBreaker
+//
+//  Created by Gal Sasson on 1/19/15.
+//
+//
+
+#include "BitmapTextButton.h"
+
+
+void BitmapTextButton::setup(const string &_label)
+{
+	label = _label;
+	setSize(label.length()*8 + 10, 20);
+
+	bDrawBorder = true;
+	bDrawBackground = true;
+
+	borderColor = ofColor(0);
+	bgColor = ofColor(255);
+	labelColor = ofColor(0);
+}
+
+void BitmapTextButton::draw()
+{
+	ofSetColor(bgColor);
+	ofFill();
+	ofDrawRectangle(0, 0, getWidth(), getHeight());
+
+	ofSetColor(labelColor);
+	ofDrawBitmapString(label, 5, getHeight()-5);
+
+	ofSetColor(borderColor);
+	ofNoFill();
+	ofDrawRectangle(0, 0, getWidth(), getHeight());
+}
+
---
+++ src/components/BitmapTextButton.h
@@ -0,0 +1,43 @@
+//
+//  BitmapTextButton.h
+//  AnimationBreaker
+//
+//  Created by Gal Sasson on 1/19/15.
+//
+//
+
+#ifndef __AnimationBreaker__BitmapTextButton__
+#define __AnimationBreaker__BitmapTextButton__
+
+#include <stdio.h>
+#include "ofMain.h"
+
+#include "ofxUINode.h"
+
+class BitmapTextButton : public ofxUINode
+{
+public:
+
+	void setup(const string& label);
+
+	void setBackground(bool set) { bDrawBackground = set; }
+	void setBorder(bool set) { bDrawBorder = set; }
+
+	void setLabelColor(const ofColor& c) { labelColor = c; }
+	void setBGColor(const ofColor& c) { bgColor = c; }
+	void setBorderColor(const ofColor& c) { borderColor = c; }
+
+	void draw();
+
+private:
+
+	bool bDrawBackground;
+	bool bDrawBorder;
+
+	ofColor borderColor;
+	ofColor bgColor;
+	ofColor labelColor;
+
+	string label;
+};
+#endif /* defined(__AnimationBreaker__BitmapTextButton__) */
