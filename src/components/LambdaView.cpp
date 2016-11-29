3d23a390c0c0c48bd261ec5ffa2fd0c6c05ce6ef
Gal Sasson <gal@localprojects.com>
2016-11-29 15:01:06 -0500

    add LambdaView

---
+++ src/components/LambdaView.cpp
@@ -0,0 +1,96 @@
+//
+//  LambdaView.cpp
+//  ofxInterface
+//
+//  Created by Gal Sasson on 12/17/15.
+//
+//
+
+#include "LambdaView.h"
+
+namespace ofxInterface
+{
+
+LambdaView::LambdaView()
+{
+	ofAddListener(eventTouchDown, this, &LambdaView::onTouchDown);
+	ofAddListener(eventTouchMove, this, &LambdaView::onTouchMove);
+	ofAddListener(eventTouchUp, this, &LambdaView::onTouchUp);
+}
+
+void LambdaView::setDrawFunction(std::function<void(void)> _func)
+{
+	drawFunc = _func;
+}
+
+void LambdaView::setUpdateFunction(std::function<void(float)> _func)
+{
+	updateFunc = _func;
+}
+
+void LambdaView::setTouchDownFunction(std::function<void(ofxInterface::TouchEvent&)> _func)
+{
+	touchDownFunc = _func;
+}
+
+void LambdaView::setTouchMoveFunction(std::function<void(ofxInterface::TouchEvent&)> _func)
+{
+	touchMoveFunc = _func;
+}
+
+void LambdaView::setTouchUpFunction(std::function<void(ofxInterface::TouchEvent&)> _func)
+{
+	touchUpFunc = _func;
+}
+
+void LambdaView::setContainsFunction(std::function<bool(const ofVec3f& global)> _func)
+{
+	containsFunc = _func;
+}
+
+void LambdaView::draw()
+{
+	if (drawFunc) {
+		drawFunc();
+	}
+}
+
+void LambdaView::update(float dt)
+{
+	if (updateFunc) {
+		updateFunc(dt);
+	}
+}
+
+bool LambdaView::contains(const ofVec3f& global)
+{
+	if (containsFunc) {
+		return containsFunc(global);
+	}
+	else {
+		return Node::contains(global);
+	}
+}
+
+void LambdaView::onTouchDown(TouchEvent& event)
+{
+	if (touchDownFunc) {
+		touchDownFunc(event);
+	}
+}
+
+void LambdaView::onTouchMove(TouchEvent& event)
+{
+	if (touchMoveFunc) {
+		touchMoveFunc(event);
+	}
+}
+
+void LambdaView::onTouchUp(TouchEvent& event)
+{
+	if (touchUpFunc) {
+		touchUpFunc(event);
+	}
+}
+
+}
---
+++ src/components/LambdaView.h
@@ -0,0 +1,47 @@
+//
+//  LambdaView.h
+//  ofxInterface
+//
+//  Created by Gal Sasson on 12/17/15.
+//
+//
+
+#ifndef LambdaView_h
+#define LambdaView_h
+
+#include "ofMain.h"
+#include "ofxInterface.h"
+
+namespace ofxInterface
+{
+
+class LambdaView : public Node
+{
+public:
+	LambdaView();
+	void setDrawFunction(std::function<void(void)> _func);
+	void setUpdateFunction(std::function<void(float)> _func);
+	void setTouchDownFunction(std::function<void(ofxInterface::TouchEvent&)> _func);
+	void setTouchMoveFunction(std::function<void(ofxInterface::TouchEvent&)> _func);
+	void setTouchUpFunction(std::function<void(ofxInterface::TouchEvent&)> _func);
+	void setContainsFunction(std::function<bool(const ofVec3f& global)> _func);
+	void update(float dt);
+	void draw();
+	bool contains(const ofVec3f& global);
+	
+private:
+	std::function<void(void)> drawFunc;
+	std::function<void(float)> updateFunc;
+	std::function<void(ofxInterface::TouchEvent&)> touchDownFunc;
+	std::function<void(ofxInterface::TouchEvent&)> touchMoveFunc;
+	std::function<void(ofxInterface::TouchEvent&)> touchUpFunc;
+	std::function<bool(const ofVec3f&)> containsFunc;
+
+	void onTouchDown(TouchEvent& event);
+	void onTouchMove(TouchEvent& event);
+	void onTouchUp(TouchEvent& event);
+};
+
+}
+
+#endif /* LambdaView_h */
